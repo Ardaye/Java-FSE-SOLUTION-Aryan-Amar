f90609668aac2a6da40e3b0bd8d84b9827aaa2ac
Aryan Amar <aryanamar2005@gmail.com>
2026-06-17 11:56:36 +0530

    Added Exercise 2 Factory Method Pattern

---
+++ Exercise2/FactoryPatternTest.java
@@ -0,0 +1,12 @@
+package Exercise2;
+
+public class FactoryPatternTest {
+
+    public static void main(String[] args) {
+        Shape shape1 = ShapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = ShapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+    }
+}
